9b4afb6d0de7ade2d4a3b95300847829bd4ac425
sirnivass <sirnivass@gmail.com>
2025-02-21 22:05:40 -0300

    chore(project): ajustes gerais e configurações do projeto

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,15 +1,7 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
-
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
  *
@@ -22,10 +14,10 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=sirnivass&password=153426&useSSL=false&allowPublicKeyRetrieval=true");
+            System.out.println("Conexão com o banco de dados estabelecida com sucesso!");
+                    } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar ao banco de dados: " + erro.getMessage());
         }
         return conn;
     }
